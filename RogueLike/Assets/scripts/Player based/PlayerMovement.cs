62ce926290fda20a6ff33da0e8b5c724d4debe59
Steef Weijdeven <413985@student.fontys.nl>
2019-10-29 13:12:32 +0100

    added new sheet

--- RogueLike/Assets/scripts/Player based/PlayerMovement.cs
+++ RogueLike/Assets/scripts/Player based/PlayerMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode, SelectionBase]
+[SelectionBase]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _MoveDelay = .2f;
@@ -72,3 +72,17 @@ public class PlayerMovement : MonoBehaviour
 //◙ ≈ ╔ ↑ ╒ τ ≤ │ ¬
 //♂ ╛ ╩ ↓ ╓ Φ ⌠ ┤ º
 //♀ ┐ ╦ → ╫ Θ ⌡ ╡ I
+
+//A B C D E F G H I J K L M N O P Q
+//R S T U V W X Y Z a b c d e f g h 
+//i j k l m n o p q r s t u v w x y 
+//z 1 2 3 4 5 6 7 8 9 0 ! @ # $ % ^ 
+//& * ( ) - = _ + ` ~ : ; { } [ ] | 
+//\ < > , . ? / ☺ ♪ └ ╠ ← ╪ Ω ÷ ╢ ☻ 
+//♫ ┴ ═ ∟ ┘ δ ≈ ╖ ♥ ☼ ┬ ╬ ↔ ┌ ∞ ° ╕ 
+//♦ ► ├ ╧ ▲ █ φ ∙ ╣ ♣ ◄ ─ ╨ ▼ ▄ ε ╗ 
+//║ ♠ ↕ ┼ ╤ # ▌ ∩ ╝ ¤ • ‼ ╞ § ╥ ▐ ≡ 
+//░ † ◘ ¶ ╟ ▬ ╙ ▀ ± ▒ ‡ ○ ‰ ╚ ↨ ╘ µ 
+//≥ ▓ ⌐ ◙ ≈ ╔ ↑ ╒ τ ≤ │ ¬ ♂ ╛ ╩ ↓ ╓ 
+//Φ ⌠ ┤ º ♀ ┐ ╦ → ╫ Θ ⌡ ╡ I
+
