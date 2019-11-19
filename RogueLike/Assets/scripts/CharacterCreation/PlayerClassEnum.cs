09ac4980ed2aa4a0539f8c44692f42245d432199
Matthijs Richel <matthijs.richel022@gmail.com>
2019-11-19 14:29:18 +0100

    Added playerCreation 'n stuff

---
+++ RogueLike/Assets/scripts/CharacterCreation/PlayerClassEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.scripts.CharacterCreation
+{
+    public enum PlayerClassEnum
+    {
+        Warrior = 1,
+        Mage = 2,
+        Ranger = 3
+    }
+}
--- RogueLike/Assets/scripts/misc/CharacterCreation.cs
+++ RogueLike/Assets/scripts/misc/CharacterCreation.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class createsprites : MonoBehaviour
+public class CharacterCreation : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
