ab8eb6ab147cbc685c96bcfecda91409606516ac
Steef Weijdeven <413985@student.fontys.nl>
2019-12-03 09:20:53 +0100

    made change to protection level
    the rest is unity doing what unity does

--- RogueLike/Assets/scripts/MainMenu/MainMenu.cs
+++ RogueLike/Assets/scripts/MainMenu/MainMenu.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public GameObject settingsPanel;
+    [SerializeField] private GameObject settingsPanel;
 
     public void StartGame()
     {
